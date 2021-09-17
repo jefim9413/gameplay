6870e2905fb117da4c5867f19217644eb2ce9b19
Fulano de Tal <jefferson.jsm3444@gmail.com>
2021-09-17 00:03:35 -0300

    atualização 17/09

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { 
-  View,
-  Text
-} from 'react-native';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import { SignIn } from './src/Signin';
+import AppLoading from 'expo-app-loading';
 
 export default function App(){
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+  
   return(
-    <SignIn/>
+    <SignIn />
   );
 }
